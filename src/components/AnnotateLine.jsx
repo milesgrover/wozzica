7d945b6760825c48e6318be8c6f246902ffb7c3e
Miles Grover <miles@milesgrover.com>
2018-10-03 14:30:17 -0700

    getting closer on line

--- src/components/AnnotateLine.jsx
+++ src/components/AnnotateLine.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import '../styles/AnnotateLine.css';
 
-const lineDrawWidth = 7;
+const canvAdjust = 10;
 
 class AnnotateLine extends Component {
     constructor(props) {
@@ -23,49 +23,81 @@ class AnnotateLine extends Component {
 
         const dotX = this.props.dotCoords.x + 1;
         const dotY = this.props.dotCoords.y + 1;
-        const labelX = this.props.labelCoords.x - this.props.labelDimensions.w / 2;
         const labelY = this.props.labelCoords.y + this.props.labelDimensions.h / 2;
+        const labelX = this.props.labelCoords.x - this.props.labelDimensions.w / 2;
 
-        const lineLeft = Math.min(dotX, labelX);
         const lineTop = Math.min(dotY, labelY);
+        const lineLeft = Math.min(dotX, labelX);
         const lineRight = Math.max(dotX, labelX);
         const lineBottom = Math.max(dotY, labelY);
-        // const lineBoxHeight = lineBottom - lineTop + lineDrawWidth;
-        const lineBoxHeight = Math.max(lineBottom - lineTop, lineDrawWidth);
-        // const lineBoxWidth = lineRight - lineLeft + lineDrawWidth;
-        const lineBoxWidth = Math.max(lineRight - lineLeft, lineDrawWidth);
-        const lineStyle = {
-            top: lineTop + 'px',
-            left: lineLeft + 'px'
+        const lineBoxHeight = lineBottom - lineTop;
+        const lineBoxWidth = lineRight - lineLeft;
+
+        let startX, startY, endX, endY, positionTop, positionLeft;
+        if (dotY < labelY && dotX < labelX) {
+            console.log('bottom right');
+            startY = 0;
+            endY = lineBoxHeight + canvAdjust / 2;
+            startX = 0;
+            endX = lineBoxWidth + canvAdjust / 2;
+            positionTop = lineTop - canvAdjust / 2;
+            positionLeft = lineLeft - canvAdjust / 2;
+        } else if (dotY < labelY && labelX < dotX){
+            console.log('bottom left');
+            startY = -canvAdjust / 2;
+            endY = lineBoxHeight + canvAdjust / 2;
+            startX = lineBoxWidth + canvAdjust / 2;
+            endX = -canvAdjust / 2;
+            positionTop = lineTop + canvAdjust / 2;
+            positionLeft = lineLeft - canvAdjust / 2;
+        } else if (labelY < dotY && dotX < labelX) {
+            console.log('top right');
+            startY = lineBoxHeight + canvAdjust / 2;
+            endY = -canvAdjust / 2;
+            startX = -canvAdjust / 2;
+            endX = lineBoxWidth + canvAdjust / 2;
+            positionTop = lineTop - canvAdjust / 2;
+            positionLeft = lineLeft + canvAdjust / 2;
+        } else if (labelY < dotY && labelX < dotX) {
+            console.log('top left');
+            startY = lineBoxHeight + canvAdjust / 2;
+            endY = -canvAdjust / 2;
+            startX = lineBoxWidth + canvAdjust / 2;
+            endX = -canvAdjust / 2;
+            positionTop = lineTop - canvAdjust / 2;
+            positionLeft = lineLeft - canvAdjust / 2;
+        }
+        // if (dotX < labelX) {
+        //     startX = -canvAdjust / 2;
+        //     endX = lineBoxWidth + canvAdjust / 2;
+        // } else {
+        //     startX = lineBoxWidth + canvAdjust / 2;
+        //     endX = -canvAdjust / 2;
+        // }
+
+        const lineBoxStyle = {
+            top: positionTop + 'px',
+            left: positionLeft + 'px'
         }
 
-        // determine whether the dot (start) is top, right, bottom, or left
-        if (dotX < labelX) {
-            // label to the right of dot
-        }
-        let startX = dotX < labelX ? 0 : lineBoxWidth;
-        let endX = dotX >= labelX ? 0 : lineBoxWidth;
-        let startY = dotY < labelY ? 0 : lineBoxHeight + lineDrawWidth / 2;
-        let endY = dotY >= labelY ? 0 : lineBoxHeight + lineDrawWidth / 2;
+        canvas.setAttribute('width', lineBoxWidth + canvAdjust);
+        canvas.setAttribute('height', lineBoxHeight + canvAdjust);
+        canvas.style.top = lineBoxStyle.top;
+        canvas.style.left = lineBoxStyle.left;
 
-        canvas.setAttribute('width', lineBoxWidth);
-        canvas.setAttribute('height', lineBoxHeight);
-        canvas.style.top = lineStyle.top;
-        canvas.style.left = lineStyle.left;
-
-        console.log(`startX: ${startX}`)
-        console.log(`startY: ${startY}`)
-        console.log(`endX: ${endX}`)
-        console.log(`endY: ${endY}`)
+        // console.log(`startX: ${startX}`)
+        // console.log(`startY: ${startY}`)
+        // console.log(`endX: ${endX}`)
+        // console.log(`endY: ${endY}`)
 
         const lineCtx = canvas.getContext('2d');
         lineCtx.beginPath();
         lineCtx.moveTo(startX, startY);
         lineCtx.lineTo(endX, endY);
-        lineCtx.lineWidth = lineDrawWidth;
+        lineCtx.lineWidth = 7;
         lineCtx.strokeStyle = '#FFFFFF';
         lineCtx.stroke();
-        lineCtx.lineWidth = Math.floor(lineDrawWidth / 2);
+        lineCtx.lineWidth = 3;
         lineCtx.strokeStyle = '#000000';
         lineCtx.stroke();
     }
