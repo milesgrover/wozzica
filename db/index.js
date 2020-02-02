ab1f77c1b99aea0a5ef3edf458b31968ecd0ea02
gingermostly <gingermostly@gmail.com>
2020-02-01 23:23:45 -0500

    create mongoose schemas and models

--- db/index.js
+++ db/index.js
@@ -10,3 +10,50 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', ()=> {
     console.log('connected, yay!')
 });
+
+const annotateSchema = new mongoose.Schema({
+    dx: {
+        type: Number, required: true
+    },
+    dy: {
+        type: Number, required: true
+    },
+    uid: { 
+        type: String, required: true 
+    },
+    lx: {
+        type: Number, required: true
+    },
+    ly: {
+        type: Number, required: true
+    },
+    ltext: {
+        type: String, required: true
+    },
+    lw: {
+        type: Number, required: true
+    },
+    lh: {
+        type: Number, required: true
+    }
+});
+const thingSchema = new mongoose.Schema({
+    id: { 
+        type: String, required: true 
+    },
+    tags: {
+        type: [String], required: true
+    },
+    image: { 
+        type: String, required: true
+     },
+    name: { 
+        type: String, required: true 
+    },
+    annotations: [ annotateSchema ]
+});
+
+const Thing = new mongoose.model('Thing', thingSchema);
+const Annotation = new mongoose.model('Annotation', annotateSchema);
+
+module.exports = { thingSchema, Thing, annotateSchema, Annotation }
